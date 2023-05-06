6d534a28ba54982da04fce7d15eae30179b9bb48
Mohammedaljaafari2019 <124896261+Mohammedaljaafari2019@users.noreply.github.com>
2023-05-07 01:22:55 +0300

    registerStudent is done

--- src/main/java/com/example/sweproject/Admin.java
+++ src/main/java/com/example/sweproject/Admin.java
@@ -1,11 +1,21 @@
 package com.example.sweproject;
+
+import java.util.ArrayList;
 import java.util.LinkedList;
+
 public class Admin {
     String name;
     int ID;
+    ArrayList<User> users;
 
     public Admin(String name) {
         this.name = name;
         this.ID = (int) Math.random() * 100000;
+    }
+
+    public void registerStudent(String Name, int id, LinkedList<Tournament> tournament) {
+        User user = new User(name, id, tournament);
+        users.add(user);
+
     }
 }
